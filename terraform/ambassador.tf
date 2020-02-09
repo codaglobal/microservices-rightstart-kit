2153ca1aa1f20afeb8a2f1fa15d9d7b6344ec180
Arvind Muthuraman <arvind.muthuraman@coda.global>
2020-02-09 17:20:35 +0530

    add dependson condition to consul and ambassador

--- terraform/ambassador.tf
+++ terraform/ambassador.tf
@@ -4,22 +4,26 @@ data "helm_repository" "datawire" {
   url  = "https://www.getambassador.io"
 }
 resource "helm_release" "ambassador" {
-  name    = "ambassador"
-  chart = "datawire/ambassador"
-  namespace = "ambassador"
+  name       = "ambassador"
+  chart      = "datawire/ambassador"
+  namespace  = "ambassador"
   repository = data.helm_repository.datawire.metadata.0.name
+
+  depends_on = [
+    google_container_node_pool.node-pool-1
+  ]
 }
 
 resource "helm_release" "consul_resolver" {
-  name    = "consul-resolver"
-  chart   = "../helm-charts/ambassador-consul-resolver"
+  name      = "consul-resolver"
+  chart     = "../helm-charts/ambassador-consul-resolver"
   namespace = "ambassador"
   depends_on = [
     helm_release.ambassador
   ]
 
   set {
-    name = "consul.host"
+    name  = "consul.host"
     value = format("%s-consul-server.%s.svc.cluster.local", helm_release.consul.name, helm_release.consul.namespace)
   }
 }
--- terraform/consul.tf
+++ terraform/consul.tf
@@ -5,35 +5,39 @@ locals {
 
 
 resource "helm_release" "consul" {
-  name  = "consul"
-  chart = local.chart_path
+  name      = "consul"
+  chart     = local.chart_path
   namespace = "consul"
-	timeout = 600
+  timeout   = 600
   # values = [
   #   file(format("%s/%s", local.chart_path, local.values_file_name))
   # ]
 
 
   set_string {
-    name = "global.datacenter"
+    name  = "global.datacenter"
     value = "microservices-dc-1"
   }
 
   # this is for development purposes only
   set_string {
-    name = "ui.service.type"
+    name  = "ui.service.type"
     value = "LoadBalancer"
   }
 
   set {
-    name = "syncCatalog.enabled"
+    name  = "syncCatalog.enabled"
     value = "true"
   }
 
   set {
-    name = "connectInject.enabled"
+    name  = "connectInject.enabled"
     value = "true"
   }
+
+  depends_on = [
+    google_container_node_pool.node-pool-1
+  ]
 }
 
 # resource "helm_release" "consul" {
